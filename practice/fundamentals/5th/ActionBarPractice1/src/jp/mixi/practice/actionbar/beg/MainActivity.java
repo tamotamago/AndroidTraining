1c5931c4a692c9b3fe5fa7ca4f4ce2d6a595e9fd
koya.tamura <koya.tamura@mixi.co.jp>
2013-05-08 13:30:18 +0900

    implement tab navigation

--- practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
+++ practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
@@ -2,22 +2,54 @@
 package jp.mixi.practice.actionbar.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class MainActivity extends SherlockActivity implements TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // タブナビゲーションモードに設定
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // タブを作成して追加。タブの選択・解除・再選択をハンドリングするコールバックの TabListener をセットしないと実行時例外でクラッシュする
+        getSupportActionBar().addTab(getSupportActionBar().newTab().setText("Tab1").setTabListener(this));
+        getSupportActionBar().addTab(getSupportActionBar().newTab().setText("Tab2").setTabListener(this));
+        getSupportActionBar().addTab(getSupportActionBar().newTab().setText("Tab3").setTabListener(this));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	getSupportMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+ // タブナビゲーションの Tab が選択された時のコールバック
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+    	Log.d("tab", "selected"+tab.getText());
+        Toast.makeText(this, tab.getText(), Toast.LENGTH_SHORT).show();
+    }
 
+    // タブナビゲーションの Tab が選択解除された時のコールバック
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+    	Log.d("tab", "unselected");
+    }
+
+    // タブナビゲーションの Tab が再度選択された時のコールバック
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    	Log.d("tab", "reselected");
+    }
 }
