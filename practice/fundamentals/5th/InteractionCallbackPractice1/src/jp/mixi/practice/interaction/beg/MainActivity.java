cc48db2f6116d1632da80073b3635aef57be7d05
koya.tamura <koya.tamura@mixi.co.jp>
2013-05-08 14:01:20 +0900

    implement option menu

--- practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
+++ practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
@@ -1,23 +1,66 @@
 
 package jp.mixi.practice.interaction.beg;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+public class MainActivity extends SherlockActivity implements OnNavigationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+     // List Navigation のリストに表示するもの
+        List<String> list = new ArrayList<String>();
+        list.add("Navi Menu 1");
+        list.add("Navi Menu 2");
+        list.add("Navi Menu 3");
+        // ナビゲーションモードを List Navigation に設定
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        // List Navigation の準備
+        getSupportActionBar().setListNavigationCallbacks(
+                new ArrayAdapter<String>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1, list),
+                this);
+        // タイトルを表示しないようにする
+        getSupportActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+    	// TODO Auto-generated method stub
+    	return super.onPrepareOptionsMenu(menu);
+    }
+    
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getSupportMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// TODO Auto-generated method stub
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+    	return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+    	// TODO Auto-generated method stub
+    	return false;
+    }
 }
