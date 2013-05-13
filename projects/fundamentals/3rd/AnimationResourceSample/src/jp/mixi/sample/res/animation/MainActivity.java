4d091d23e8606f07cfe8e098e5e5971afee30d48
Koya Tamura <denden_k_t_810@yahoo.co.jp>
2013-05-13 22:24:50 +0900

    implement animation

--- projects/fundamentals/3rd/AnimationResourceSample/src/jp/mixi/sample/res/animation/MainActivity.java
+++ projects/fundamentals/3rd/AnimationResourceSample/src/jp/mixi/sample/res/animation/MainActivity.java
@@ -2,23 +2,22 @@ package jp.mixi.sample.res.animation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        View textView = findViewById(R.id.TextView1);
-
-        // Animation Resource を読み込んで、Animation オブジェクトを得る
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.sample_animation);
-        // Animation オブジェクトを View に渡して、アニメーションを開始する
-        textView.startAnimation(animation);
+        
+        Button button = (Button)findViewById(R.id.Button1);
+        button.setOnClickListener(this);
     }
 
     @Override
@@ -27,5 +26,13 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    
+    @Override
+    public void onClick(View v) {
+    	// TODO Auto-generated method stub
+    	Log.d("button", "click");
+        View textView = findViewById(R.id.TextView1);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.sample_animation);
+        textView.startAnimation(animation);
+    }		
 }
