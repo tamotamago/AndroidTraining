97d0eaf8dfbcbc50393be5f407f133d741f50e3b
koya.tamura <koya.tamura@mixi.co.jp>
2013-05-16 15:41:44 +0900

    implement sql practice

--- practice/fundamentals/10th/SQLitePractice/src/jp/mixi/practice/sqlite/MainActivity.java
+++ practice/fundamentals/10th/SQLitePractice/src/jp/mixi/practice/sqlite/MainActivity.java
@@ -2,7 +2,12 @@
 package jp.mixi.practice.sqlite;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteAbortException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -43,6 +48,15 @@ public class MainActivity extends Activity {
 
     private void insert() {
         // TODO:ここにinsert処理を実装してください
+        BookOpenHelper bookOpenHelper = new BookOpenHelper(this);
+        SQLiteDatabase db = bookOpenHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(Book.COLUMN_NAME_BOOK_TITLE, "title1");
+        contentValues.put(Book.COLUMN_NAME_BOOK_PRICE, "price1");
+        contentValues.put(Book.COLUMN_NAME_BOOK_PUBLISHER, "publisher1");
+        long rowID = db.insert(Book.BOOK_TABLE_NAME, null, contentValues);
+        Log.d(this.getLocalClassName(), "insert");
     }
 
     private void delete() {
@@ -55,6 +69,24 @@ public class MainActivity extends Activity {
 
     private void query() {
         // TODO:ここにquery処理を実装してください
+        BookOpenHelper bookOpenHelper = new BookOpenHelper(this);
+        SQLiteDatabase db = bookOpenHelper.getReadableDatabase();
+        String[] projection = {
+                Book._ID,
+                Book.COLUMN_NAME_BOOK_TITLE,
+                Book.COLUMN_NAME_BOOK_PRICE,
+                Book.COLUMN_NAME_BOOK_PUBLISHER
+        };
+
+//        String selection = Book._ID + " = ?";
+//        String[] selectionArgs = {
+//                "1"
+//        };
+        Cursor cursor = db.query(Book.BOOK_TABLE_NAME, projection, null, null, null, null, null);
+        while (cursor.moveToNext()){
+            long itemID = cursor.getLong(cursor.getColumnIndexOrThrow(Book._ID));
+            Log.d(this.getLocalClassName(), Long.toString(itemID));
+        }
     }
 
     @Override
